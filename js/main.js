3717dd89c8ec35a2950c0d9916338a3604bc7437
sjurate <rasaujuratei@gmail.com>
2022-07-14 15:26:25 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const divDOM = document.querySelector('div');
+const btnPlusDOM = document.querySelector('button:nth-of-type(1)');
+const btnMinusDOM = document.querySelector('button:nth-of-type(2)');
+
+let number = 0;
+divDOM.textContent = number;
