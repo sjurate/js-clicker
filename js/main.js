540b4b30ea7a20a862881856a438b6ebc8f8a4f7
sjurate <rasaujuratei@gmail.com>
2022-07-14 20:35:12 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/*
+const wikiDOM = document.querySelector('a');
+
+wikiDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('SURPRIZE!! 😈');
+})
+
+*/
 
 const divDOM = document.querySelector('div');
 const btnPlusDOM = document.querySelector('button:nth-of-type(1)');
@@ -5,3 +14,29 @@ const btnMinusDOM = document.querySelector('button:nth-of-type(2)');
 
 let number = 0;
 divDOM.textContent = number;
+
+function padidinti() {
+   divDOM.textContent = ++number ;
+}
+
+function sumazinti() {
+    divDOM.textContent = --number ;
+ }
+
+ function padidinti10(event) {
+    event.preventDefault();
+    number += 10 ; 
+    divDOM.textContent = number ;
+ }
+ 
+ function sumazinti10(event) {
+    event.preventDefault();
+    number -= 10 ; 
+    divDOM.textContent = number ;
+  }
+ 
+ btnPlusDOM.addEventListener('click', padidinti);
+btnMinusDOM.addEventListener('click', sumazinti);
+
+btnPlusDOM.addEventListener('contextmenu', padidinti10);
+btnMinusDOM.addEventListener('contextmenu', sumazinti10);
